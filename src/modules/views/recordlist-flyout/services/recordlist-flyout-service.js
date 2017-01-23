d30165aae65ec8c89237664bf7edca664ab47843
J <U6040534@ten.thomsonreuters.com>
2017-01-23 11:58:57 +0530

    Angular service to fetch more services when more button is clicked in flyout

--- src/modules/views/recordlist-flyout/services/recordlist-flyout-service.js
+++ src/modules/views/recordlist-flyout/services/recordlist-flyout-service.js
@@ -1,12 +1,43 @@
 'use strict';
-(function(angular){
-  	angular
-    .module('rv.views.RecordListFlyOutModule')
-    .service('recordListFlyOutService', RecordListFlyOutService);
+(function(angular) {
+  angular
+  .module('rv.views.RecordListFlyOutModule')
+  .service('recordListFlyOutService', RecordListFlyOutService);
 
-  RecordListFlyOutService.$inject = [];
+  RecordListFlyOutService.$inject = ['$q', '$http'];
 
-  function RecordListFlyOutService(){
-  
+  function RecordListFlyOutService($q, $http) {
+    var getRecords = function (category, resultsetId, offset, size, totalRecords) {
+      var defer = $q.defer();
+      if (angular.isDefined(category) && angular.isDefined(resultsetId) && angular.isDefined(offset) && angular.isDefined(size) && angular.isDefined(totalRecords)) {
+        var config = {
+          method: 'GET',
+          url: 'http://localhost:8085/RESTServer/getMore',
+          params: {
+            category: category,
+            resultsetId: resultsetId,
+            offset: offset,
+            size: size,
+            totalRecords: totalRecords,
+
+          }
+        };
+
+        $http(config).then(function (response) {
+          defer.resolve(response.data);
+        }, function(reject) {
+          defer.reject(reject.data);
+        });
+
+        return defer.promise;
+      }else {
+        defer.reject('Request params missing');
+        return defer.promise;
+      }
+    };
+
+    return {
+      getRecords: getRecords
+    };
   }
 })(angular);
