3d675997c07964fe5323599403778e73e91bb7a5
J <U6040534@ten.thomsonreuters.com>
2017-01-23 12:14:49 +0530

    Moving out flyout constants to a dedicated module

--- src/modules/views/recordlist-flyout/services/recordlist-flyout-service.js
+++ src/modules/views/recordlist-flyout/services/recordlist-flyout-service.js
@@ -4,15 +4,15 @@
   .module('rv.views.RecordListFlyOutModule')
   .service('recordListFlyOutService', RecordListFlyOutService);
 
-  RecordListFlyOutService.$inject = ['$q', '$http'];
+  RecordListFlyOutService.$inject = ['$q', '$http', 'FLYOUT_CONFIG'];
 
-  function RecordListFlyOutService($q, $http) {
+  function RecordListFlyOutService($q, $http, FLYOUT_CONFIG) {
     var getRecords = function (category, resultsetId, offset, size, totalRecords) {
       var defer = $q.defer();
       if (angular.isDefined(category) && angular.isDefined(resultsetId) && angular.isDefined(offset) && angular.isDefined(size) && angular.isDefined(totalRecords)) {
         var config = {
           method: 'GET',
-          url: 'http://localhost:8085/RESTServer/getMore',
+          url: FLYOUT_CONFIG.MORE_URL,
           params: {
             category: category,
             resultsetId: resultsetId,
